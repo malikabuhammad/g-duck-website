7e9d4e6e0c0b491ee41e4b23592390b4e62fa8c7
Malik <Malik@MALIK>
2025-12-06 17:10:59 +0300

    a

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/g-duck-website',
   reactStrictMode: true,
   images: {
     formats: ['image/webp'],
