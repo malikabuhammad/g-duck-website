83456dc1b90e7ec4f7a56651595aaa2137c8907c
Malik <Malik@MALIK>
2025-12-06 17:07:38 +0300

    deployments

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   reactStrictMode: true,
   images: {
     formats: ['image/webp'],
     domains: [],
+    unoptimized: true,
   },
   compress: true,
   poweredByHeader: false,
